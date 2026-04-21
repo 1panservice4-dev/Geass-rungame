242c8b2892c97851267e4055cd0986c3bb05eb91
1panservice4-dev <1panservice4@gmail.com>
2026-04-22 08:30:48 +0900

    fix syntax error

--- geass_run.py
+++ geass_run.py
@@ -38,6 +38,7 @@ def play_geass_run():
         @keyframes shake { 0%, 100% { transform: translate(0,0); } 25% { transform: translate(5px, 5px); } 75% { transform: translate(-5px, -5px); } }
         .shaking { animation: shake 0.1s 5; }
     </style>
+    """
     </head>
     <body onload="document.getElementById('game-area').focus()">
     <div id="game-area" tabindex="0">
